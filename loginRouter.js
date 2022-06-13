3eb299274c4a8b170b4eadf61adc6b81d14cd844
lucas_almeida <lucas.almdiniz@gmail.com>
2022-06-13 19:37:03 -0300

    feat: Requisito 4 - Adicione as validações para o endpoint /login

--- loginRouter.js
+++ loginRouter.js
@@ -3,14 +3,41 @@ const crypto = require('crypto');
 
 const router = express.Router();
 
-function login(req, res) {
-  // const { email, password } = req.body;
+function validateEmail(req, res, next) {
+  const { email } = req.body;
+  const regexEmail = /\S+@\S+\.com/g;
 
+  if (!email) { 
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+
+  if (!regexEmail.test(email)) { 
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+
+  next();
+}
+
+function validatePassword(req, res, next) {
+  const { password } = req.body;
+
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+
+  next();
+}
+
+function login(_req, res) {
   const token = crypto.randomBytes(8).toString('hex');
 
   res.status(200).json({ token });
 }
 
-router.post('/', login);
+router.post('/', validateEmail, validatePassword, login);
 
 module.exports = router;
